10b5b6540e794b7995751f76807310d01cdbd15f
aliasghar5124 <92795509+aliasghar5124@users.noreply.github.com>
2023-08-24 13:08:06 +0500

    First

---
+++ Quiz_app_flutter/lib/configs/configs.dart
@@ -0,0 +1,6 @@
+export 'themes/app_colors.dart';
+export 'themes/custom_text_styles.dart';
+export 'themes/ui_parameters.dart';
+export 'themes/app_icons_icons.dart';
+export 'themes/dark_theme.dart';
+export 'themes/light_theme.dart';
